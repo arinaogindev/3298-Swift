4992467e50c3a2632750ce9bddc2bff741c79db0
SBabchukTM <babchuk.serhii@trident-media.agency>
2025-09-12 11:45:44 +0300

    init

---
+++ Leprechaun'sGenius/App/Leprechaun_sGeniusApp.swift
@@ -0,0 +1,17 @@
+//
+//  Leprechaun_sGeniusApp.swift
+//  Leprechaun'sGenius
+//
+//  Created by Serhii Babchuk on 12.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct Leprechaun_sGeniusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            LGLaunchView()
+        }
+    }
+}
---
+++ Pods/Target Support Files/Pods-Leprechaun'sGenius/Pods-Leprechaun'sGenius-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Leprechaun_sGenius : NSObject
+@end
+@implementation PodsDummy_Pods_Leprechaun_sGenius
+@end
